d9d772bc1813ff561f0e1d9b563d0cdffbf2312b
albertotovar <albertoleyestovar@gmail.com>
2025-01-04 21:48:03 +0900

    call from backend

--- src/Graph.js
+++ src/Graph.js
@@ -1,47 +1,69 @@
 import { gql, GraphQLClient } from 'graphql-request';
+import axios from 'axios';
 
-const endPoint = "https://api.studio.thegraph.com/query/98873/bet-graph-test/version/latest";
+const API_ENDPOINT = "https://4318-146-185-26-150.ngrok-free.app";
 
 export const getBetInfo = async (_roundId) => {
-    const client = new GraphQLClient(endPoint);
-    const filter = _roundId ? `(where: {_roundId: "${_roundId}"})` : "";
-    const query = gql`
-        {
-            betPlaceds${filter} {
-                id
-                _roundId
-                _address
-                _betValue
-                _betAmount
-            }
-        }`;
-    const response = await client.request(query);
-    return response;
-
+    try {
+        const res = await axios.post(API_ENDPOINT + "/bet-info", {
+            roundId: _roundId
+        });
+        return res.data;
+    } catch (error) {
+        // return { betPlaceds: [], betRoundFinisheds: [] };
+        console.log(error);
+    }
 }
 
 export const getBetHistory = async (_address) => {
-    const client = new GraphQLClient(endPoint);
-    const query = gql`
-        {
-            betPlaceds {
-                id
-                _roundId
-                _address
-                _betValue
-                _betAmount
-            },
-            betRoundFinisheds {
-                _roundId
-                _winningValue
-            },
-            claimedRewards(where: {_address: "${_address}"}) {
-                _roundId
-                _address
-                _rewardAmount
-            }
-        }`;
-    const response = await client.request(query);
-    // console.log(response);
-    return response;
+    const res = await axios.post(API_ENDPOINT + "/bet-history", {
+        address: _address
+    });
+    console.log(res.data);
+    return res.data;
 }
+// const endPoint = "https://api.studio.thegraph.com/query/98873/bet-graph-test/version/latest";
+
+// export const getBetInfo = async (_roundId) => {
+//     const client = new GraphQLClient(endPoint);
+//     const filter = _roundId ? `(where: {_roundId: "${_roundId}"})` : "";
+//     const query = gql`
+//         {
+//             betPlaceds${filter} {
+//                 id
+//                 _roundId
+//                 _address
+//                 _betValue
+//                 _betAmount
+//             }
+//         }`;
+//     const response = await client.request(query);
+//     return response;
+
+// }
+
+// export const getBetHistory = async (_address) => {
+//     const client = new GraphQLClient(endPoint);
+//     const query = gql`
+//         {
+//             betPlaceds {
+//                 id
+//                 _roundId
+//                 _address
+//                 _betValue
+//                 _betAmount
+//             },
+//             betRoundFinisheds {
+//                 _roundId
+//                 _winningValue
+//             },
+//             claimedRewards(where: {_address: "${_address}"}) {
+//                 _roundId
+//                 _address
+//                 _rewardAmount
+//             }
+//         }`;
+//     const response = await client.request(query);
+//     // console.log(response);
+//     return response;
+// }
